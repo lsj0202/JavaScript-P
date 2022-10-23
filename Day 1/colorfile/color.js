c288496a0e117b5110767377cea2b0f0c562e161
lsj0202 <w1004123@gmail.com>
2022-10-23 22:22:25 +0900

    first commit

---
+++ Day 1/colorfile/color.js
@@ -0,0 +1,4 @@
+var c = document.querySelector('#heading'); //document.querySelector은 css 서택자이다.
+        c.onclick = function(){ //버튼때문에 onclick을 씀
+            c.style.color ="red";//querySelector문과 연결되는 문장임 
+        }
