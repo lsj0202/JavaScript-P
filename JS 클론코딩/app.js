f6083d4a778dc46edbf8cb34689d33f119230144
lsj0202 <w1004123@gmail.com>
2022-11-15 21:38:28 +0900

    .

---
+++ JS 클론코딩/app.js
@@ -0,0 +1,9 @@
+console.log('lalalalalal');
+console.log(5+2);
+console.log(5.3+5.2);
+
+const a = 5;
+const b=
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
---
+++ 개념 공부/Day 9/array.js
@@ -0,0 +1,13 @@
+const wed = "wed";
+const thu = "thu";
+const thv = "thv";
+
+const day = ["mon","tue","wed","thu","fri"];
+
+const sus = [1,2,"hello",false,true,null];
+
+console.log(day[3]);
+
+day.push("sun");
+
+console.log(day);
