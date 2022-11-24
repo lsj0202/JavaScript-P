ad5fb8a3ab57607b08e5de5526b4ae1bad71204a
lsj0202 <w1004123@gmail.com>
2022-11-24 20:40:44 +0900

    ADD :: Login

--- JS 클론코딩/app.js
+++ JS 클론코딩/app.js
@@ -1,9 +1,26 @@
-const loginInput = document.querySelector('#login-form input');
-const loginButton= document.querySelector('#login-form button');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-    ocnsole.log('click!!!');
+const HIDDEN_CLASSNAME ="hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault(); 
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null ){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    
 }
 
 
-loginButton.addEventListener("click", )
